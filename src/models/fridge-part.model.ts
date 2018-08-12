50ee225739577a860036c9710b96afde59838198
Julio Silva <juliocmdsilva@gmail.com>
2018-08-12 01:52:21 -0300

    Basic setup for status page

--- src/models/fridge-part.model.ts
+++ src/models/fridge-part.model.ts
@@ -1,4 +1,25 @@
 export class FridgePart {
     open: boolean;
+    name: "geladeira" | "freezer";
     temperature: number;
+
+    constructor(open: boolean, name: "geladeira" | "freezer", temperature: number) {
+        this.open = open;
+        this.name = name;
+        this.temperature = temperature;
+    }
+
+    get status(): string {
+        let statusStr = "";
+        switch (this.name) {
+            case "geladeira": {
+                statusStr += `Sua ${this.name} está `;
+                return statusStr + (this.open ? "aberta" : "fechada");
+            }
+            case "freezer": {
+                statusStr += `Seu ${this.name} está `;
+                return statusStr + (this.open ? "aberto" : "fechado");
+            }
+        }
+    }
 }
--- src/pages/status/status.ts
+++ src/pages/status/status.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { GlobalStateProvider } from '../../providers/global-state/global-state';
+import { FridgePart } from '../../models/fridge-part.model';
 
 @Component({
   selector: 'page-status',
@@ -8,9 +9,14 @@ import { GlobalStateProvider } from '../../providers/global-state/global-state';
   providers: [GlobalStateProvider]
 })
 export class StatusPage {
+  fridge: FridgePart;
 
   constructor(public navCtrl: NavController, public globals: GlobalStateProvider) {
+    this.fridge = this.mockFridge();
+  }
 
+  mockFridge(): FridgePart {
+    return new FridgePart(true, "geladeira", 75);
   }
 
 }
